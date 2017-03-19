265766765ec66a0f5f110bc6b0fe14190543dbce
Laura Maillet <50ftwomen@gmail.com>
2017-03-19 14:43:30 -0700

    working facebook oauth

--- models/user.js
+++ models/user.js
@@ -11,7 +11,7 @@ var userSchema = mongoose.Schema({
 		id       : String,
 		token    : String,
 		email    : String,
-		name     : String,
+		name     : String
 	},
 	bars       : [Bar.schema]
 });
