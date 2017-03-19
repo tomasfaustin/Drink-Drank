e929695b86e62cb97eb5a1773746a39a6e2b9999
Jake Hawkinson <jakehawk1095@gmail.com>
2017-03-18 22:50:20 -0700

    starting to build base of project

--- server.js
+++ server.js
@@ -1,7 +1,37 @@
-var express = require('express'),
-		path		= require('path'),
-		logger  = require('morgan'),
-		cookieParser = require('cookie-parser'),
-		bodyParser   = require('body-parser');
+var express 			= require('express'),
+		app						= express(),
+		mongoose 			= require('mongoose'),
+		passport			= require('passport'),
+		flash					= require('connect-flash'),
+		path					= require('path'),
+		morgan  			= require('morgan'),
+		cookieParser	= require('cookie-parser'),
+		bodyParser  	= require('body-parser'),
+		session		  	= require('express-session');
+
+mongoose.connect('mongodb://localhost/test-db-p3');
+
+app.use(morgan('dev'));
+app.use(cookieParser);
+app.use(bodyParser);
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.static(__dirname + '/public'));
+
+require('./config/passport')(passport);
+
+app.use(function (req, res, next) {
+	global.user = req.user;
+	next();
+});
 
 
+
+var routes = require('./config/routes');
+app.use(routes);
+
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+	console.log('connected on port', port);
+})
