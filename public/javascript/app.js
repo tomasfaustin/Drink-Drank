816b31861c6488c4bfc49a7a60417c49adb9d8c0
Laura Maillet <50ftwomen@gmail.com>
2017-03-20 15:04:33 -0700

    added yelp search to app.js

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,10 +1,13 @@
 var $visitedBars
 var $nonVisitedBar
+var $form
+var $searchTerm
+var yelpInfo
 var Yelp = require('yelp-api-v3')
 
 var yelp = new Yelp ({
   app_id: process.env.YELP_ID,
-  app_secret: process.env.YELP_SECRET 
+  app_secret: process.env.YELP_SECRET
 })
 
 
@@ -13,4 +16,17 @@ var yelp = new Yelp ({
 $(document).ready(function(){
   $visitedBars = $('#visitedBars')
   $nonVisitedBar = $('nonVisitedBar')
+  $form = $('#search')
+  $searchTerm = $('#searchTerm')
+
+  $form.on('submit', function(e) {
+    yelp.search({term: `${$searchTerm.val()}`, categories: 'bars', location: '90291', limit: 5})
+      .then(function (data) {
+        yelpInfo = data
+      })
+
+
+  })
+
+
 })
