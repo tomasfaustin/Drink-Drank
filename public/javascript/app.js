c5d2aab1e7960afcf60be21ccac91ed2af70d941
Laura Maillet <50ftwomen@gmail.com>
2017-03-20 15:48:58 -0700

    changed app.js, yelp api header etc

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -3,13 +3,13 @@ var $nonVisitedBar
 var $form
 var $searchTerm
 var yelpInfo
-var Yelp = require('yelp-api-v3')
 
-var yelp = new Yelp ({
-  app_id: process.env.YELP_ID,
-  app_secret: process.env.YELP_SECRET
-})
-
+//
+// var yelp = new Yelp ({
+//   app_id: process.env.YELP_ID,
+//   app_secret: process.env.YELP_SECRET
+// })
+//
 
 
 
@@ -17,16 +17,24 @@ $(document).ready(function(){
   $visitedBars = $('#visitedBars')
   $nonVisitedBar = $('nonVisitedBar')
   $form = $('#search')
-  $searchTerm = $('#searchTerm')
+
 
   $form.on('submit', function(e) {
-    yelp.search({term: `${$searchTerm.val()}`, categories: 'bars', location: '90291', limit: 5})
+    $searchTerm = $('#searchTerm')
+    $.ajax({
+      url: 'https://api.yelp.com/v3/businesses/search?term=' + $searchTerm + '&categories=bars&location=90291&limit=5',
+      headers: {'Authorization': 'Bearer fLveZdK9txmlU-Anng1LZlZAn2GLQyDZUPSjdWad-XXer34dGkZ7QkI3R590v9S1ESdnmWQPhNLNafb7-I6hclnw2j46EvS3ogU-SWqkf4UijCLZyC6TZnbcesTKWHYx'}
       .then(function (data) {
         yelpInfo = data
+        console.log(data)
       })
-
-
+      .catch(function (err) {
+        console.log(err);
+      })
+    })
   })
+
+
 
 
 })
