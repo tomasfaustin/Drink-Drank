a0b0093f5f1561a46348c50a3df8259586b21a20
Laura Maillet <50ftwomen@gmail.com>
2017-03-20 16:36:47 -0700

    modified index.js in controller, took away price parameter

--- controllers/index.js
+++ controllers/index.js
@@ -14,7 +14,7 @@ function index(req, res) {
 
 function search(req, res) {
   console.log(req.query)
-  yelp.search({term: req.query.term, categories: 'bars', location: '90210', price: '1,2,3', limit: 10})
+  yelp.search({term: req.query.term, categories: 'bars', location: '90210', limit: 50})
   .then(function (data) {
       console.log(data);
     res.json(data)
