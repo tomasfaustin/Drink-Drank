1bfee1ac6ef5d142f4f1153838df36eca45ec21c
Laura Maillet <50ftwomen@gmail.com>
2017-03-21 12:20:51 -0700

    added location to bar model

--- models/bar.js
+++ models/bar.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose')
 
 var barSchema = new mongoose.Schema({
   name				: String,
-  image_url  	: String
+  image_url  	: String,
+  location    : String
 })
 
 var Bar = mongoose.model('Bars', barSchema);
