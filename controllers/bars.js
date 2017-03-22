d7bd94d031fce4ada6e7b59b00cc819dbbef2d3c
Jake Hawkinson <jakehawk1095@gmail.com>
2017-03-22 10:59:10 -0700

    cleaned up bars controller and routes

--- controllers/bars.js
+++ controllers/bars.js
@@ -1,70 +1,50 @@
 var Bar  = require('../models/bar'),
     User = require('../models/user');
 
-
-// function index(req, res) {
-//     User.find({}, function(err, bars) {
-//         if (err) throw err;
-//
-//         res.json(bars);
-//     });
-// }
-
 function createBar(req, res) {
-    console.log('req.body is', req.body);
-    console.log(req.user);
+
     User.findById(req.user._id, function(err, user) {
         if (err) throw err;
 
-        console.log(user);
         user.bars.push({
             name: req.body.name,
             image_url: req.body.image_url,
             location: req.body.location
-        })
+        });
 
         user.save(function(err, user) {
             if (err) throw err;
 
             res.redirect('/index');
-        })
-    })
+        });
 
-
-    // var newBar = new Bar(req.body);
-
-    // newBar.save(function(err, savedBar) {
-    //     if (err) throw err;
-
-    //     res.json(savedBar);
-    // });
+    });
 }
 
 function updateBar(req, res) {
   var id = req.params.id;
-  console.log(req.user)
+
   User.findById(req.user._id, function(err, user) {
     if (err) throw err
-  var bar = user.bars.id(id)
-  console.log(bar)
-    // Bar.findById(id, function(err, bar) {
-    //   if (err || !bar) throw err
+    
+    var bar = user.bars.id(id);
 
-      bar.visited = !bar.visited
-      user.save(function(err, updatedUser) {
-        if (err) throw err
-        var updatedBar = updatedUser.bars.id(id)
-        res.json(updatedBar)
-        console.log("Updated?")
-      })
-    // })
-  })
+    bar.visited = !bar.visited
+    user.save(function(err, updatedUser) {
+      if (err) throw err
+      var updatedBar = updatedUser.bars.id(id)
+      res.json(updatedBar)
+      console.log("Updated?")
+    });
+
+  });
 }
 
 function deleteBar(req, res) {
-  var id = req.params.id;
+  var id     = req.params.id,
+  var userId = req.user._id;
 
-  User.findById(req.user._id, function(err, user) {
+  User.findById(userId, function(err, user) {
     if (err) throw err;
 
     var bar = user.bars.id(id);
@@ -77,10 +57,6 @@ function deleteBar(req, res) {
     })
   })
 }
-
-
-
-
 
 module.exports = {
     createBar: createBar,
